a08b2e05307505ab54819616250db1ecc64a7733
Bekir - CDI <b.burakcebecii@gmail.com>
2022-11-22 23:16:42 +0100

    api url updated.. .env dateiadded..

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -6,12 +6,11 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
-
   const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
   useEffect(() => {
     (async () => {
-      const response = (await axios.get(`${BASE_API_URL}meetups`)).data;
+      const response = (await axios.get(`${BASE_API_URL}/meetups`)).data;
       setLoadedMeetups(response.meetups);
       console.log(loadedMeetups);
       setIsLoading(false);
@@ -23,7 +22,7 @@ export const AppProvider = ({ children }) => {
       isFavorite: !meetup.isFavorite,
     };
 
-    fetch(`${BASE_API_URL}meetups/${meetup._id}`, {
+    fetch(`${BASE_API_URL}/meetups/${meetup._id}`, {
       method: "PUT",
       body: JSON.stringify(meetupData),
       headers: { "Content-Type": "application/json" },
